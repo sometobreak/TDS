b827f8e6ef6deb1f1399d5b7ea4cf03ab860cb17
sometobreak <sometobreak@yandex.ru>
2024-09-06 00:07:18 +0300

    Revert "restructuring project part 2"
    This reverts commit d8ee26fe0aa2b3e8106de76ca09c8215007933d8.

--- Source/TDS/Game/TDSGameMode.cpp
+++ Source/TDS/Game/TDSGameMode.cpp
@@ -11,7 +11,7 @@ ATDSGameMode::ATDSGameMode()
 	PlayerControllerClass = ATDSPlayerController::StaticClass();
 
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Characters/Base/Blueprints/BP_Character"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/Characters/BP_Character"));
 	if (PlayerPawnBPClass.Class != nullptr)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
