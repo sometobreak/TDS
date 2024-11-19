19d8979f6493426caba735ad7499c957758c1652
sometobreak <sometobreak@yandex.ru>
2024-11-19 17:47:24 +0300

    add base health component

---
+++ Source/TDS/Character/TDSHealthComponent.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TDSHealthComponent.h"
+
+// Sets default values for this component's properties
+UTDSHealthComponent::UTDSHealthComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UTDSHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UTDSHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+float UTDSHealthComponent::GetCurrentHealth()
+{
+	return Health;
+}
+
+void UTDSHealthComponent::ReceiveDamage(float Damage)
+{
+	Health -= Damage;
+	if (Health <= 0.0f)
+	{
+		DeadEvent();
+	}
+	OnHealthChange.Broadcast(Health);
+}
+
+void UTDSHealthComponent::DeadEvent()
+{
+}
+
---
+++ Source/TDS/Character/TDSHealthComponent.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "TDSHealthComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChange, float, Health);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDead);
+
+USTRUCT(BlueprintType)
+struct FStatsParam
+{
+	GENERATED_BODY()
+
+
+};
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TDS_API UTDSHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UTDSHealthComponent();
+
+	UPROPERTY(BlueprintAssignable, EditAnywhere, BlueprintReadWrite, Category = "Heatlh")
+	FOnHealthChange OnHealthChange;
+	UPROPERTY(BlueprintAssignable, EditAnywhere, BlueprintReadWrite, Category = "Heatlh")
+	FOnDead OnDead;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	float Health = 100.0f;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Health")
+	float GetCurrentHealth();
+	UFUNCTION(BlueprintCallable, Category = "Health")
+	void ReceiveDamage(float Damage);
+	UFUNCTION(BlueprintCallable, Category = "Health")
+	void DeadEvent();
+};
